cf07de4549b072970addd97c0febb67da810cbfa
Cutterchoice44 <adrian@akdcasting.com>
2025-06-07 15:11:31 +1200

    Update get_archives.php

--- get_archives.php
+++ get_archives.php
@@ -1,9 +1,12 @@
 <?php
 header('Content-Type: application/json');
-$dataFile = __DIR__ . '/archives.json';
+
+$artistId = isset($_GET['artistId'])
+    ? preg_replace('/[^a-zA-Z0-9_-]/','',$_GET['artistId'])
+    : '';
+$dataFile = __DIR__ . ($artistId ? "/archives_{$artistId}.json" : '/archives.json');
 if (!file_exists($dataFile)) {
-  echo json_encode([]);
-  exit;
+    echo json_encode([]);
+    exit;
 }
 echo file_get_contents($dataFile);
-?>
