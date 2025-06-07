c17c879cdb0a261f37fb65b0c08b80b8853ced96
Cutterchoice44 <adrian@akdcasting.com>
2025-06-07 03:39:54 +0000

    Update get_archives.php

--- get_archives.php
+++ get_archives.php
@@ -1,13 +1,20 @@
 <?php
+// Prevent any caching
+header('Cache-Control: no-cache, no-store, must-revalidate');
+header('Pragma: no-cache');
+header('Expires: 0');
+
 header('Content-Type: application/json');
 
 $artistId = filter_input(INPUT_GET,'artistId',FILTER_SANITIZE_STRING);
 $artistId = $artistId ? preg_replace('/[^a-zA-Z0-9_-]/','',$artistId) : '';
-$dataFile = __DIR__ . ($artistId ? "/archives_{$artistId}.json" : '/archives.json');
+$dataFile = __DIR__ . ($artistId
+    ? "/archives_{$artistId}.json"
+    : '/archives.json');
 
 if (!file_exists($dataFile)) {
-  echo json_encode([]);
-  exit;
+    echo json_encode([]);
+    exit;
 }
 
 echo file_get_contents($dataFile);
